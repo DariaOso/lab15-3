1185e177aed99acb80cf33527f409de774d9bfba
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-12-12 13:51:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args){
         ArrList s= new ArrList();
         s.go();
-        LinkList l = new LinkList();
-        l.go();
     }
 }
+
